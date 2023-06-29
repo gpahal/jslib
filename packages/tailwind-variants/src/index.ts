66144d5893b92a7d6e2c3ca459879221f53f9502
Garvit Pahal <g10pahal@gmail.com>
2023-06-29 17:43:37 +0530

    Add hocus-visible tailwind variant

--- packages/tailwind-variants/src/index.ts
+++ packages/tailwind-variants/src/index.ts
@@ -2,4 +2,5 @@ import plugin from 'tailwindcss/plugin'
 
 export default plugin(({ addVariant }) => {
   addVariant('hocus', ['&:hover', '&:focus'])
+  addVariant('hocus-visible', ['&:hover', '&:focus-visible'])
 })
