9971fcba4738e4935a354d89dc32fa27c23f2e90
Garvit Pahal <g10pahal@gmail.com>
2023-07-17 10:11:39 +0530

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,27 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   root: true,
-  extends: ['turbo', 'eslint:recommended', 'plugin:vitest-globals/recommended', 'prettier'],
+  extends: [
+    'turbo',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vitest-globals/recommended',
+    'prettier',
+  ],
   env: {
     es2022: true,
     node: true,
     browser: true,
   },
-  parser: '@babel/eslint-parser',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 'latest',
-    requireConfigFile: false,
+    warnOnUnsupportedTypeScriptVersion: true,
+    project: './tsconfig.eslint.json',
   },
-  plugins: ['unicorn'],
+  plugins: ['@typescript-eslint', 'unicorn'],
   rules: {
     'no-unused-vars': 'off',
     'no-restricted-imports': [
@@ -31,39 +39,25 @@ const config = {
       },
     ],
     'unicorn/prefer-node-protocol': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'no-type-imports' }],
+    '@typescript-eslint/no-misused-promises': [2, { checksVoidReturn: { attributes: false } }],
+    '@typescript-eslint/no-floating-promises': ['error'],
+    '@typescript-eslint/prefer-nullish-coalescing': ['off'],
+    '@typescript-eslint/unbound-method': ['off'],
   },
   overrides: [
     {
-      files: ['*.ts'],
-      extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 'latest',
-        warnOnUnsupportedTypeScriptVersion: true,
-        project: './tsconfig.eslint.json',
-      },
-      plugins: ['@typescript-eslint', 'unicorn'],
-      rules: {
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          {
-            argsIgnorePattern: '^_',
-            varsIgnorePattern: '^_',
-            caughtErrorsIgnorePattern: '^_',
-            destructuredArrayIgnorePattern: '^_',
-          },
-        ],
-        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-        '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'no-type-imports' }],
-        '@typescript-eslint/no-misused-promises': [2, { checksVoidReturn: { attributes: false } }],
-        '@typescript-eslint/no-floating-promises': ['error'],
-        '@typescript-eslint/prefer-nullish-coalescing': ['off'],
-        '@typescript-eslint/unbound-method': ['off'],
-      },
-    },
-    {
-      files: ['**/*.test.{js,mjs,cjs,ts}'],
+      files: ['**/*.test.{ts,tsx}'],
       env: {
         'vitest-globals/env': true,
       },
