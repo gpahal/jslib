61b210a805c4926db6edf4d87c7f0bf3fcde6f2d
Garvit Pahal <g10pahal@gmail.com>
2023-07-18 09:51:16 +0530

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   root: true,
+  reportUnusedDisableDirectives: true,
   extends: [
     'turbo',
     'eslint:recommended',
@@ -21,7 +22,7 @@ const config = {
     warnOnUnsupportedTypeScriptVersion: true,
     project: './tsconfig.eslint.json',
   },
-  plugins: ['@typescript-eslint', 'unicorn'],
+  plugins: ['@typescript-eslint', 'import', 'unicorn'],
   rules: {
     'no-unused-vars': 'off',
     'no-restricted-imports': [
@@ -30,15 +31,15 @@ const config = {
         patterns: ['..*'],
       },
     ],
-    'unicorn/filename-case': [
+    '@typescript-eslint/no-unused-vars': [
       'error',
       {
-        cases: {
-          kebabCase: true,
-        },
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
       },
     ],
-    'unicorn/prefer-node-protocol': 'error',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
@@ -50,10 +51,29 @@ const config = {
     ],
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'no-type-imports' }],
-    '@typescript-eslint/no-misused-promises': [2, { checksVoidReturn: { attributes: false } }],
+    '@typescript-eslint/no-misused-promises': ['error', { checksVoidReturn: { attributes: false } }],
     '@typescript-eslint/no-floating-promises': ['error'],
     '@typescript-eslint/prefer-nullish-coalescing': ['off'],
     '@typescript-eslint/unbound-method': ['off'],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-absolute-path': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-named-default': 'error',
+    'import/no-self-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-amd': 'error',
+    'import/no-webpack-loader-syntax': 'error',
+    'import/no-anonymous-default-export': 'error',
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          kebabCase: true,
+        },
+      },
+    ],
+    'unicorn/prefer-node-protocol': 'error',
   },
   overrides: [
     {
@@ -63,7 +83,6 @@ const config = {
       },
     },
   ],
-  reportUnusedDisableDirectives: true,
 }
 
 module.exports = config
