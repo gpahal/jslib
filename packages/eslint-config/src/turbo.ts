4d05fa9795f5eda736d792122f28fadbb0498790
Garvit Pahal <g10pahal@gmail.com>
2024-10-01 10:56:41 +0530

    Update dependencies

--- packages/eslint-config/src/turbo.ts
+++ packages/eslint-config/src/turbo.ts
@@ -3,17 +3,14 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import eslintPluginTurbo from 'eslint-plugin-turbo'
-import { config } from 'typescript-eslint'
+import { config, type ConfigWithExtends } from 'typescript-eslint'
 
 import type { Config } from './base'
 
 export default config({
   plugins: {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     turbo: eslintPluginTurbo,
   },
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
   settings: eslintPluginTurbo.configs.recommended.settings,
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
   rules: eslintPluginTurbo.configs.recommended.rules,
-}) as Config
+} as ConfigWithExtends) as Config
